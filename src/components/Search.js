dcf9ed92550a956f01480c254b7532895df4850b
Jay Davis <jdlikesbsbl@gmail.com>
2025-02-27 09:59:27 -0600

    adds search feature

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,9 @@
-import React from "react";
+import { all } from "axios";
+import React, { useState } from "react";
 
-function Search() {
+function Search({ list, setList }) {
+  const [allPlants, setAllPlants ] = useState(list); 
+
   return (
     <div className="searchbar">
       <label htmlFor="search">Search Plants:</label>
@@ -8,7 +11,13 @@ function Search() {
         type="text"
         id="search"
         placeholder="Type a name to search..."
-        onChange={(e) => console.log("Searching...")}
+        onChange={(e) => {
+          const query = e.target.value.toLowerCase();
+          const results = query
+          ? allPlants.filter(el => el.name.toLowerCase().includes(query))
+          : allPlants;
+          setList(results)
+        }}
       />
     </div>
   );
