3785dfa7f693f6064008f557fb234c9c683e4c82
Jay Davis <jdlikesbsbl@gmail.com>
2025-02-26 21:34:31 -0600

    renders all the db.json plants on load

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
-  return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
-  );
+
+function PlantList({list, setList}) {
+ useEffect(() => {
+   fetch("http://localhost:6001/plants")
+     .then(resp => resp.json())
+     .then(data => setList(data))
+ }, [setList])
+
+
+ return (
+   <ul className="cards">{list.map((obj) => {
+     return (
+     <PlantCard
+     key={obj.id}
+     name={obj.name}
+     image={obj.image}
+     price={obj.price}
+     />
+     );
+   })}</ul>
+ );
 }
+
 
 export default PlantList;
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
+
 function PlantPage() {
+  const [ list, setList ] = useState([])
+
+
   return (
-    <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
-    </main>
+	<main>
+  	<NewPlantForm />
+  	<Search />
+  	<PlantList list={list} setList={setList}/>
+	</main>
   );
 }
 
+
 export default PlantPage;
+
